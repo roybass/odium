16e54d435dfa2a7a4d9f645aa6275cec488bf5a9
obroybass <rbass@outbrain.com>
2018-08-14 19:26:40 +0300

    Starting ot work on auth

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var bodyParser = require('body-parser');
 
 var routes = require('./server/routes/index');
 var fit = require('./server/routes/fit');
-
+var passport = require('passport');
+var OAuth2Strategy = require('passport-oauth2');
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +24,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'static')));
 
+app.use(passport.initialize());
+passport.use(new OAuth2Strategy({
+    authorizationURL: 'https://login.eveonline.com/oauth/authorize?scope=esi-location.read_location.v1',
+    tokenURL: 'https://https://login.eveonline.com/oauth/token',
+    clientID: '<CLIENT_ID>',
+    clientSecret: '<CLIENT_SECRET>',
+    callbackURL: "http://localhost:3000/auth/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log('', accessToken, refreshToken, profile);
+    return cb(null, '');
+  }
+));
+
 app.use('/', routes);
 app.use('/fit', fit);
+app.use('/login', passport.authenticate('oauth2'));
+
+app.get('/auth/callback', passport.authenticate('oauth2', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
